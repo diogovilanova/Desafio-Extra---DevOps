8bde8eeb198ebe9724e630bd09c3f08940f95abc
Diogo Vila Nova <diogo.vila27@gmail.com>
2024-07-22 17:27:54 -0300

    updates

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+set -e
 # Instalar dependências
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
@@ -16,11 +18,22 @@ sudo apt-get update
 # Instalar o Docker
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 # Instalação do Docker Compose
+echo "Instalando Docker Compose..."
 sudo curl -L "https://github.com/docker/compose/releases/download/$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep tag_name | cut -d '"' -f 4)/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 # Permissões de execução para o Docker Compose
 sudo chmod +x /usr/local/bin/docker-compose
 # Baixar o docker-compose.yml do GitHub
 sudo curl -L -o /home/azureuser/docker-compose.yml https://github.com/diogovilanova/Desafio-Extra---DevOps/raw/main/docker-compose.yml
-# Rodar o docker-compose
+# Verificar se o arquivo docker-compose.yml foi baixado
+echo "Arquivo docker-compose.yml:"
+ls -la /home/azureuser/docker-compose.yml
+# Mudar para o diretório correto
+echo "Mudando para o diretório /home/azureuser..."
 cd /home/azureuser
+# Verificar se a mudança de diretório foi bem-sucedida
+echo "Diretório atual após cd:"
+pwd
+# Rodar o docker-compose
+echo "Iniciando containers com Docker Compose..."
 sudo docker-compose up -d
+echo "Script concluído."
