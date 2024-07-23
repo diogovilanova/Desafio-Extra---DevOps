ee76318f3b2f8d5e08fba5066cfdd1091e11adb6
Diogo Vila Nova <diogo.vila27@gmail.com>
2024-07-23 10:36:13 -0300

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -22,21 +22,11 @@ echo "Instalando Docker Compose..."
 sudo curl -L "https://github.com/docker/compose/releases/download/$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep tag_name | cut -d '"' -f 4)/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 # Permissões de execução para o Docker Compose
 sudo chmod +x /usr/local/bin/docker-compose
-# Baixar o Dockerfile
-
 # Baixar o docker-compose.yml e Dockerfile do GitHub
 sudo curl -L -o /home/azureuser/Dockerfile https://github.com/diogovilanova/Desafio-Extra---DevOps/raw/main/Dockerfile
 sudo curl -L -o /home/azureuser/docker-compose.yml https://github.com/diogovilanova/Desafio-Extra---DevOps/raw/main/docker-compose.yml
-# Verificar se o arquivo docker-compose.yml foi baixado
-echo "Arquivo docker-compose.yml:"
-ls -la /home/azureuser/docker-compose.yml
 # Mudar para o diretório correto
-echo "Mudando para o diretório /home/azureuser..."
 cd /home/azureuser
-# Verificar se a mudança de diretório foi bem-sucedida
-echo "Diretório atual após cd:"
-pwd
 # Rodar o docker-compose
-echo "Iniciando containers com Docker Compose..."
 sudo docker-compose up -d
 echo "Script concluído."
